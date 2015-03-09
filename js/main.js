28316e5dc0900b8645867246b24b3b1345ae6330
Matthew Reuther <matthew.reuther@gmail.com>
2015-03-09 14:32:32 -0500

    added some firebase functionality get, put, and post

--- js/main.js
+++ js/main.js
@@ -1,62 +1,104 @@
 angular
   .module('tas', [])
-  .controller('TasController', function () {
+  .controller('TasController', function ($scope, $http) {
     var vm = this;
 
-    vm.data = [
-      {
-        nickName: 'TAdam',
-        name: 'Adam',
-        firstName: 'Adam',
-        lastName: 'Kèésecker',
-        current: true
-      },
-      {
-        nickName: 'ZAdam',
-        name: 'Adam',
-        firstName: 'Zöe',
-        lastName: 'Ames',
-        current: true
-      },
-      {
-        nickName: 'JuAdam',
-        name: 'Adam',
-        firstName: 'Juan',
-        lastName: 'Rødrįguež',
-        current: true
-      },
-      {
-        nickName: 'BrAdam',
-        name: 'Adam',
-        firstName: 'Brian',
-        lastName: 'Hiått',
-        current: false
-      },
-      {
-        nickName: 'BAdam',
-        name: 'Adam',
-        firstName: 'Adam',
-        lastName: 'Barñhærd',
-        current: false
-      }
+    vm.cohortOptions = [
+      'N/A',
+      'One',
+      'Two',
+      'Three',
+      'Four',
+      'Five',
+      'Six',
+      'Seven',
+      'Eight',
+      'Nine',
+      'Ten'
     ];
+
+    // $http.put('https://angular-demo-tas.firebaseio.com/tas.json',
+    //  [
+    //   {
+    //     nickName: 'TAdam',
+    //     name: 'Adam',
+    //     firstName: 'Adam',
+    //     lastName: 'Kèésecker',
+    //     current: true,
+    //     cohort: 5
+    //   },
+    //   {
+    //     nickName: 'ZAdam',
+    //     name: 'Adam',
+    //     firstName: 'Zöe',
+    //     lastName: 'Ames',
+    //     current: true,
+    //     cohort: 6
+    //   },
+    //   {
+    //     nickName: 'JuAdam',
+    //     name: 'Adam',
+    //     firstName: 'Juan',
+    //     lastName: 'Rødrįguež',
+    //     current: true,
+    //     cohort: 6
+    //   },
+    //   {
+    //     nickName: 'BrAdam',
+    //     name: 'Adam',
+    //     firstName: 'Brian',
+    //     lastName: 'Hiått',
+    //     current: false,
+    //     cohort: 6
+    //   },
+    //   {
+    //     nickName: 'BAdam',
+    //     name: 'Adam',
+    //     firstName: 'Adam',
+    //     lastName: 'Barñhærd',
+    //     current: false,
+    //     cohort: 6
+    //   }
+    // ])
+
+    $http
+      .get('https://angular-demo-tas.firebaseio.com/tas.json')
+      .success(function (data) {
+        vm.data = data;
+      });
 
     vm.addTA = function () {
       vm.newTA.name = 'Adam';
       vm.newTA.nickName = vm.newTA.firstName[0].toUpperCase() + 'Adam';
 
-      vm.data.push(vm.newTA);
-      _clearNewTA();
+      console.log(vm.newTA);
+      $http.post('https://angular-demo-tas.firebaseio.com/tas.json', vm.newTA)
+        .success(function (data) {
+          vm.data[data.name] = vm.newTA;
+          _clearNewTA();
+        })
+    };
+    vm.removeTA = function (id) {
+      // var index = vm.data.indexOf(person);       //use when not posting to firebase
+      // vm.data.splice(index, 1);
+      var url = 'https://angular-demo-tas.firebaseio.com/tas/' + id + '.json';
+      $http
+        .delete(url)
+        .success(function () {
+          delete vm.data[id];
+        })
     };
 
-    vm.removeTA = function (person) {
-      var index = vm.data.indexOf(person);
-      vm.data.splice(index, 1);
+    vm.updateTA = function (id) {
+      var url = 'https://angular-demo-tas.firebaseio.com/tas/' + id + '.json';
+      $http
+        .put(url, vm.data[id]);
     };
+
 
     function _clearNewTA() {
       vm.newTA = {};
-      // vm.newTA.$setPristine();
+      $scope.newTA.$setPristine();
     }
 
   });
